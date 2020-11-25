1b9aef3ce34a45139a37ac15bc11b544e5cab672
morsstepan <morsstepan@icloud.com>
2020-11-25 10:56:44 +0300

    Add Spring Boot scheme

---
+++ src/test/java/com/github/morsstepan/ysu/moderntechnologies/eurika/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.morsstepan.ysu.moderntechnologies.eurika.eurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
